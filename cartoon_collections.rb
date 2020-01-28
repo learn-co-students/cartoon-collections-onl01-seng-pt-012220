fcbbd1cffc901d3450051202b87ff0300ea9f84e
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-28 15:39:58 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index|
+  puts "#{index + 1} #{name}"
+end
+end
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |word|
+    word.capitalize + "!"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+calls = ["puff", "go", "two"]
+def long_planeteer_calls(calls)
+  if calls.all? { |word| word.length <= 4 }
+FALSE
+else
+TRUE
+end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(words)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+ 
+ cheese_types.each do |word|
+ if words.include? word
+ return word
+ else 
+ return nil
+ end
+ end
+ end
+
