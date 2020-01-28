8f228893a7c90418bbd399fc6a14cb5d0b83401b
Xavier Perez <xperez007@gmail.com>
2020-01-28 18:30:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,53 @@
 def roll_call_dwarves# code an argument here
+  def roll_call_dwarves#["Doc", "Dopey", "Bashful",
+"Grumpy"]
+ puts = Doc, Dopey,Bashful,Grumpy
+ return
+ end
+ each_with_index
+ end# Your code here
+end
+
+def summon_captain_planet
+  long_planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  return
+end
+ summon_captain_planet(long_planeteer_calls)
+ => ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+ .summon_captain_planet
+ .collect 
+ # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls
+  short_words = ["puff", "go", "two"]
+  long_planeteer_calls(short_words)
+  #=> false 
+  
+  assorted_words =[two", "go", "industrious","bop"]
+  long_planeteer_calls(assorted_words)
+  #= true 
+  
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def find_the_cheese
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+#> "gouda"
 
-def find_the_cheese# code an argument here
+soup = [tomato soup", "cheddar", "oyster crackers", "gouda"}
+find_the_cheese(soup)
+#=> "cheddar"
+
+ingredients = ["garlic", 
+"rosemary", "bread"]
+find_the_cheese(ingredients)
+#= >nil 
+.include
+end
+code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
