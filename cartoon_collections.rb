20a6d73c709ced5a6c33c4981659e45db0023de7
ohjacksn <jacksonmo@me.com>
2020-01-30 13:52:12 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  dwarves = []
+  while array.length < 8
+     dwarves.push array.each_with_index {|item, index| puts "#{index}." + "#{item}"}
+  end
+  puts dwarves
 end
 
 def summon_captain_planet# code an argument here
